00aad4a563a577321de5a0d4808160fb05d06b53
Trosterud <sindre.reino.trosterud@gmail.com>
2019-10-29 12:09:54 +0200

    add welcome name json api functionality from stackabuse.com/deploying-a-flask-application-to-heroku/

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,9 +6,39 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
     
+@app.route("/getmsg/", methods=["GET"])
+def respond():
+    name = request.args.get("name", None)
+
+    print(f"got name {name}")
+
+    response = {}
+
+    if not name:
+        response["ERROR"] = "no name found, please send a name"
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric"
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform"
+
+    return jsonify(response)
+
+@app.route("/post/", methods=["POST"])
+def post_something():
+    param = request.form.get("name")
+    print(param)
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform",
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name"
+        })
 
 @app.route("/salvador")
-def salvado():
+def salvador():
     return "Hello, Salvador"
 
 if __name__ == "__main__":
