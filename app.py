5d63393bf2f051d7efa61a07c33291e1fe0a2e6d
Trosterud <sindre.reino.trosterud@gmail.com>
2019-10-30 12:48:16 +0100

    rename about function

--- app.py
+++ app.py
@@ -37,9 +37,11 @@ def post_something():
             "ERROR": "no name found, please send a name"
         })
 
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # Threaded option to enable multiple instances for multiple user access support
+    # app.run(threaded=True, port=5000)
