4cb82348c6dffb459309c296eb31775de96318df
Miikka Tervo <miikka.tervo@gmail.com>
2019-11-04 14:42:46 +0200

    started over, trying to deploy to heroku

--- otaniemiForeca/otaniemiForeca/urls.py
+++ otaniemiForeca/otaniemiForeca/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('hello/', include('hello.urls')),
+    path('helloWorld/', include('helloWorld.urls')),
     path('admin/', admin.site.urls),
 ]
--- otaniemiForeca/otaniemiForeca/wsgi.py
+++ otaniemiForeca/otaniemiForeca/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for otaForeca project.
+WSGI config for otaniemiForeca project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'otaForeca.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'otaniemiForeca.settings')
 
 application = get_wsgi_application()
